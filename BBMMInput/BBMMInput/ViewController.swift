fe729b67d8b3f7fc4370311a564683b0f4890401
bbmm <471693221@qq.com>
2016-11-15 20:56:41 +0800

    init program

---
+++ BBMMInput/BBMMInput/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BBMMInput
+//
+//  Created by LiYuhuan on 16/11/15.
+//  Copyright © 2016年 LiYuhuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
